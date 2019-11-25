7d9ab40772bbd0f791d38a087a70708594740ac0
Dmitry Fokin <fokind1@yandex.ru>
2019-11-25 21:32:14 +0300

    отправляется пост

--- webapp/controller/AnalysisForm.controller.js
+++ webapp/controller/AnalysisForm.controller.js
@@ -1,8 +1,12 @@
 /* global moment */
 
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "ck/trader/thirdparty/moment-with-locales"],
-  function(Controller) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "ck/trader/thirdparty/moment-with-locales"
+  ],
+  function(Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("ck.trader.controller.AnalysisForm", {
@@ -13,14 +17,44 @@ sap.ui.define(
           .getRoute("analysisForm")
           .attachMatched(this._onRouteMatched, this);
         this.getView().addStyleClass(oComponent.getContentDensityClass());
+        this.getView().setModel(new JSONModel());
       },
 
-      _onRouteMatched: function(oEvent) {},
+      _onRouteMatched: function() {},
 
       onBackPress: function() {
         this.getOwnerComponent()
           .getRouter()
           .navTo("main");
+      },
+
+      onExecutePress: function() {
+        var oRouter = this.getOwnerComponent().getRouter();
+        var oModel = this.getView().getModel();
+        $.post({
+          async: true,
+          url: "/odata/Session",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: JSON.stringify({
+            exchange: oModel.getProperty("/exchange"),
+            currency: oModel.getProperty("/currency"),
+            asset: oModel.getProperty("/asset"),
+            period: +oModel.getProperty("/period"),
+            begin: moment.utc(oModel.getProperty("/begin")).toISOString(),
+            end: moment
+              .utc(oModel.getProperty("/end"))
+              .add(1, "d")
+              .add(-1, "s")
+              .toISOString(),
+            indicators: oModel.getProperty("/indicators")
+          })
+        }).then(function(oData) {
+          oRouter.navTo("analysisShow", {
+            id: oData._id
+          });
+        });
       }
     });
   }
--- webapp/controller/AnalysisShow.controller.js
+++ webapp/controller/AnalysisShow.controller.js
@@ -1,8 +1,12 @@
 /* global moment */
 
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "ck/trader/thirdparty/moment-with-locales"],
-  function(Controller) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "ck/trader/thirdparty/moment-with-locales"
+  ],
+  function(Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("ck.trader.controller.AnalysisShow", {
@@ -13,9 +17,29 @@ sap.ui.define(
           .getRoute("analysisShow")
           .attachMatched(this._onRouteMatched, this);
         this.getView().addStyleClass(oComponent.getContentDensityClass());
+        this.getView().setModel(new JSONModel());
       },
 
-      _onRouteMatched: function(oEvent) {},
+      _onRouteMatched: function(oEvent) {
+        this._bind("/Session('" + oEvent.getParameter("arguments").id + "')");
+      },
+
+      _bind: function(sPath) {
+        console.log(sPath);
+        var oModel = new JSONModel();
+        var oView = this.getView();
+        oView.bindElement({
+          path: "data>" + sPath,
+          events: {
+            dataReceived: function() {
+              console.log(1);
+              var oBindingContext = oView.getBindingContext("data");
+              console.log(oBindingContext);
+            }
+          }
+        });
+        console.log(2);
+      },
 
       onBackPress: function() {
         this.getOwnerComponent()
